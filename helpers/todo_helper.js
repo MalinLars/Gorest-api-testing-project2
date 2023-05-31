3a9bee6c29d467b867163d104c5dc1aef123be18
Guest_Admin <guest_admin@Ambers-MacBook-Pro.local>
2023-05-31 17:52:41 +0200

    Unable to generate a random due_on date using falso. Hard-coded a due date again in todo helper file. Cleaned up code in todo.js file

--- helpers/todo_helper.js
+++ helpers/todo_helper.js
@@ -5,7 +5,7 @@ export const createRandomTodo = (userId) => {
     const todo = {
         user_id: userId,
         title: randTodo().title,
-        due_on: randTodo.due_on,
+        due_on: '2023-06-05T00:00:00.000+05:30',
         status: 'pending'
     };
     return todo;
